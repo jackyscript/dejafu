cfc966f4598de252acbdc0b615ccb276eea8029b
jackyscript <8930712+jackyscript@users.noreply.github.com>
2025-04-28 09:01:57 +0200

    update contact section

--- src/components/items/ContactItem.vue
+++ src/components/items/ContactItem.vue
@@ -12,19 +12,20 @@ defineProps({
 </script>
 <template>
   <section>
-    <slot name="icon"></slot>
-    <h3><slot name="heading"></slot></h3>
-    <p v-if="value !== undefined" />
-    <slot name="link">{{ value }}</slot>
+    <article>
+      <slot name="icon"></slot>
+      <h3><slot name="heading"></slot></h3>
+      <p v-if="value !== undefined" />
+      <slot name="link">{{ value }}</slot>
+    </article>
   </section>
 </template>
 
 <style scoped>
-
 h3 {
   line-height: 1.2;
-	padding: 0;
-	background: var(--pico-mark-gradient);
-	background-clip: text;
+  padding: 0;
+  background: var(--pico-mark-gradient);
+  background-clip: text;
 }
 </style>
